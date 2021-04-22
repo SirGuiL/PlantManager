39afb6c6019f55ee67ab32c262c4830fdb87feb6
Guilherme Lima <guibiel-10@hotmail.com>
2021-04-22 13:43:05 -0300

    Dia 2 - Página de inicio e navegação entre telas

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
+import { 
+    StyleSheet, 
+    Text, 
+    TouchableOpacity,
+    TouchableOpacityProps
+} from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps{
     title: String;
 }
 
-export function Button({ title, ...rest }: ButtonProps) {
-    return (
-        <TouchableOpacity
-            style={styles.button}
-            activeOpacity={0.7}
-            {...rest}
-        >
-            <Text style={styles.buttonText}>
+export function Button({ title, ...rest }: ButtonProps){
+    return(
+        <TouchableOpacity style={styles.container}>
+            <Text 
+                style={styles.text}
+                {...rest}
+            >
                 {title}
             </Text>
         </TouchableOpacity>
@@ -21,18 +26,16 @@ export function Button({ title, ...rest }: ButtonProps) {
 }
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
         backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
         height: 56,
-        width: 56,
-        paddingHorizontal: 10,
-        marginTop: 20
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    buttonText: {
-        color: colors.white
+    text: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading
     }
 });
